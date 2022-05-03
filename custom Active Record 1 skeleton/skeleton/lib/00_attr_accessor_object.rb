f71f56c91cd92df83ae928b36c73090ab3b9c467
miker704 <miker704@gmail.com>
2022-05-03 18:36:26 -0400

    finished atrrAccessorOject5

--- custom Active Record 1 skeleton/skeleton/lib/00_attr_accessor_object.rb
+++ custom Active Record 1 skeleton/skeleton/lib/00_attr_accessor_object.rb
@@ -1,5 +1,25 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
-  end
+
+    names.each do |nm|
+
+      define_method(nm) do
+       
+               instance_variable_get("@#{nm}")
+          
+      end
+            
+                define_method("#{nm}=") do |v|
+                      
+                    instance_variable_set("@#{nm}",v)
+
+                end
+
+    end
+
+    end
+  
+  
+  
 end
